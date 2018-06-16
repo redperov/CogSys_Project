834d1973eab476bca4a0b0143b3c926db2e2629c
redperov <redperov@gmail>
2018-06-16 21:54:43 +0300

    Added functions needed for the algorithm

--- mcts.py
+++ mcts.py
@@ -3,10 +3,11 @@ Implementation of the Monte Carlo Tree Search algorithm.
 """
 import numpy as np
 from valid_action import PythonValidActions
+from random import choice
 
 NUM_OF_SIMULATIONS = 3
 C = 1.4
-valid_actions_getter = PythonValidActions()
+valid_actions_getter = None
 
 
 # class State(object):
@@ -49,6 +50,7 @@ class Node(object):
         return self._win_score
 
     def get_all_possible_states(self):
+        # TODO to understand how to use it, watch the tutorial of the algorithm
         pass
 
 
@@ -61,12 +63,14 @@ def monte_carlo_tree_search(root):
 
     return best_child(root)
 
+
 def traverse(node):
 
     while fully_expanded(node):
         node = best_uct(node)
 
-    return pick_unvisited(node.get_children()) or node # in case no children are present / node is terminal
+    return pick_unvisited(node.get_children()) or node  # in case no children are present / node is terminal
+
 
 def fully_expanded(node):
     """
@@ -80,15 +84,30 @@ def fully_expanded(node):
 
     return True
 
+
 def best_uct(node):
 
     choices_weights = [
         (c.get_win_score() / (c.get_win_score())) + C * np.sqrt((2 * np.log(node.get_win_score()) /
                                                                  (c.get_win_score())))
-        for c in node.children
-    ]
+        for c in node.children]
 
     return node.children[np.argmax(choices_weights)]
+
+
+def pick_unvisited(node):
+
+    children = node.get_children()
+
+    # TODO if using numpy array, change to .size()
+    if len(children) == 0:
+        return None
+
+    for child in children:
+        if children.get_visit_count() == 0:
+            return child
+
+    return None
 
 
 def rollout(node):
@@ -98,18 +117,39 @@ def rollout(node):
 
     return result(node)
 
+
+def non_terminal(node):
+    # TODO implement, need to consider both available children and the max depth I want to check
+    pass
+
+
 def rollout_policy(node):
 
-    return pick_random(node.children)
+    return choice(node.children)
+
+
+def result(node):
+    # TODO implement, need to consider both available children and the max depth I want to check
+    pass
+
 
 def backpropagate(node, result):
 
-   if is_root(node):
-       return
+    # Check if node is root.
+    if node.get_parent() is None:
+        return
 
-   node.stats = update_stats(node, result)
-   backpropagate(node.parent)
+    node.stats = update_stats(node, result)
+    backpropagate(node.parent)
+
+
+def update_stats(node, result):
+    # TODO implement, need to consider both available children and the max depth I want to check
+    pass
+
 
 def best_child(node):
 
-    pick child with highest number of visits
+    # Pick the child with the highest number of visits.
+    visit_count_list = [child.get_visit_count() for child in node.get_children()]
+    return node.get_children()[np.argmax(visit_count_list)]
